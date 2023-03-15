ae86e6496a1838e0127f5764f4f1270729a33865
AMAR DEEP <amar1998deep@gmail.com>
2023-03-15 18:34:53 +0530

    vercel.json file added

--- Config/db.js
+++ Config/db.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 require("dotenv").config();
 
-const connection=mongoose.connect(process.env.mongoURL);
+const connection=mongoose.connect(process.env.mongoURL || "mongodb+srv://AmarDeep:AmarTalkDeep@cluster0.sj6zn4j.mongodb.net/knock-talk?retryWrites=true&w=majority");
 
 module.exports={connection};
